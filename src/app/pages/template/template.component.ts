f6e77ce441a5e03f4e4d7f1c2755e02479bd8e9b
Nata <anatanael437@gmail.com>
2020-03-11 22:58:02 -0300

    formularios por template terminado

---
+++ src/app/pages/template/template.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css']
+})
+export class TemplateComponent implements OnInit {
+  usuario = {
+    nombre: "Natanael",
+    apellido: "Acosta",
+    correo: "anatanael437@gmail.com",
+    pais: 'ARG',
+    genero: "M"
+  };
+  paises: any[] = [];
+
+  constructor( private paisService: PaisService ) { }
+
+  ngOnInit(): void {
+    this.paisService.getPaises()
+      .subscribe((paises:any) => {
+        this.paises = paises;
+        this.paises.unshift({
+          nombre: '[Seleccione un Pais]',
+          codigo: ''
+        });
+      });
+  }
+  
+  guardar(forma:NgForm) {
+    if (forma.invalid) {
+      Object.values( forma.controls ).forEach(control => {
+        control.markAsTouched();
+      });
+      return;
+    }
+
+    console.log(forma.value);
+  }
+}
