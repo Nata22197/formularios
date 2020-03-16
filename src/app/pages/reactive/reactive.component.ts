277df6ca268d713f928025c676e53e486d38eb47
Nata <anatanael437@gmail.com>
2020-03-16 01:07:02 -0300

    formularios asincronos terminado

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { ValidadoresService } from '../../services/validadores.service';
 
 @Component({
   selector: 'app-reactive',
@@ -6,10 +8,115 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive.component.css']
 })
 export class ReactiveComponent implements OnInit {
-
-  constructor() { }
+  
+  forma: FormGroup;
+  constructor( private formBuilder: FormBuilder,
+               private validadores: ValidadoresService ) { 
+    this.crearFormulario();
+    this.cargarDataAlFormulario();
+    this.crearListeners();
+  }
 
   ngOnInit(): void {
   }
+  
+  crearFormulario() {
+    this.forma = this.formBuilder.group({
+      nombre: ['', [Validators.required, Validators.minLength(5)] ],
+      apellido: ['', [Validators.required, this.validadores.noHerrera]],
+      correo: ['', [ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$') ]],
+      usuario: ['', ,this.validadores.existeUsuario ],
+      pass1: ['', Validators.required],
+      pass2: ['', Validators.required],
+      direccion: this.formBuilder.group({
+        distrito: ['', Validators.required],
+        ciudad: ['', Validators.required]
+      }),
+      pasatiempos: this.formBuilder.array([])
+    }, {
+      validators: this.validadores.passwordsIguales('pass1', 'pass2')
+    });
+  }
+  
+  crearListeners() {
+    // this.forma.valueChanges.subscribe(valor => { console.log({valor}); }); 
+    
+    // this.forma.statusChanges.subscribe(status => { console.log({status}); });
 
+    this.forma.get('nombre').valueChanges.subscribe(console.log);
+  }
+
+  cargarDataAlFormulario() {
+    this.forma.reset({
+      nombre: 'Juancito',
+      apellido: 'Perez',
+      correo: 'juanperez@gmail.com',
+      pass1: '123',
+      pass2: '123',
+      direccion: {
+        distrito: 'Ontario',
+        ciudad: "Ottawa"
+      }
+    });
+
+    ['Comer', 'Dormir'].forEach( valor => this.pasatiempos.push(this.formBuilder.control(valor)) )
+  }  
+
+  guardar() {
+    console.log(this.forma);
+    if (this.forma.invalid) {
+      return Object.values( this.forma.controls ).forEach(control => {
+        control.markAllAsTouched();
+      });
+    }
+
+    this.forma.reset();
+  }
+
+  agregarPasatiempo() {
+    this.pasatiempos.push(this.formBuilder.control('') );
+  }
+  
+  borrarPasatiempo(i:number) {
+    this.pasatiempos.removeAt(i);
+  }
+
+  get pasatiempos() {
+    return this.forma.get('pasatiempos') as FormArray;
+  }
+
+  get nombreNoValido() {
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched;
+  }
+
+  get apellidoNoValido() {
+    return this.forma.get('apellido').invalid && this.forma.get('apellido').touched;
+  }
+
+  get correoNoValido() {
+    return this.forma.get('correo').invalid && this.forma.get('correo').touched;
+  }
+
+  get usuarioNoValido() {
+    return this.forma.get('usuario').invalid && this.forma.get('usuario').touched;
+  }
+
+  get distritoNoValido() {
+    return this.forma.get('direccion.distrito').invalid && this.forma.get('direccion.distrito').touched;
+  }
+
+  get ciudadNoValido() {
+    return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched;
+  }
+
+  get pass1NoValido() {
+    return this.forma.get('pass1').invalid && this.forma.get('pass1').touched;
+  }
+  
+  get pass2NoValido() {
+    const pass1 = this.forma.get('pass1').value;
+    const pass2 = this.forma.get('pass2').value;
+  
+    return (pass1 === pass2) ? false : true;
+  }
 }
